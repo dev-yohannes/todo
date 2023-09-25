21527d95158428dcfd74425e73057d14bd817ee1
Yohannes Mulu <dev.yohannes@gmail.com>
2023-09-25 18:01:32 +0300

    adding edit and save button

--- script.js
+++ script.js
@@ -5,12 +5,38 @@ const ul = document.querySelector("ul");
 const mainConditional = () => {
   const newLi = document.createElement("li");
   newLi.textContent = todoInput.value;
+  localStorage.setItem("userValue", todoInput.value);
   ul.prepend(newLi);
 
   newLi.addEventListener("click", () => {
     newLi.remove();
   });
+
+  const editButton = document.createElement("button");
+  const saveButton = document.createElement("button");
+
+  newLi.addEventListener("mouseenter", () => {
+    editButton.textContent = "Edit";
+    newLi.append(editButton);
+
+    editButton.addEventListener("click", () => {
+      const newInputEdit = document.createElement("input");
+      newLi.innerHTML = newInputEdit.value;
+      newInputEdit.setAttribute(
+        "placeholder",
+        `${localStorage.getItem("userValue")}`
+      );
+      newLi.replaceWith(newInputEdit);
+
+      saveButton.textContent = "Save";
+      newLi.append(saveButton);
+    });
+  });
 };
+
+// newLi.addEventListener("mouseleave", () => {
+//   editButton.style.display = "none";
+// });
 
 const checkingEmptyInput = () => {
   if (todoInput.value === "") {
